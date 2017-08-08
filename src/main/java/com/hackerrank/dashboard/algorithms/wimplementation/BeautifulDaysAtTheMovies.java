a14b4f5e2a91302483f57426319666a67ebc31d6
Wendler Zacariotto <wendler2013@hotmail.com>
2017-08-08 01:01:42 -0300

    Commit dos novos testes realizados

---
+++ src/main/java/com/hackerrank/dashboard/algorithms/wimplementation/BeautifulDaysAtTheMovies.java
@@ -0,0 +1,24 @@
+package com.hackerrank.dashboard.algorithms.wimplementation;
+
+import java.util.Scanner;
+
+public class BeautifulDaysAtTheMovies {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int d1 = in.nextInt();
+        int d2 = in.nextInt();
+        int k = in.nextInt();
+        int db = 0;
+        while(d1<=d2){
+        	if((d1-reverse(d1))%k==0)
+        		db++;
+        	d1++;
+        }System.out.println(db);
+   
+    }
+	
+	static int reverse(int value){
+		StringBuffer sb = new StringBuffer(String.valueOf(value));
+		return Integer.parseInt(sb.reverse().toString());
+	}
+}
