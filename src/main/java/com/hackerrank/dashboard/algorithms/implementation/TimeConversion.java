a0981a0a48eaca1d194a6d44e4a75eb581a543fe
Wendler Zacariotto <wendler2013@hotmail.com>
2018-02-02 19:14:59 -0200

    Adding new stuffs

---
+++ src/main/java/com/hackerrank/dashboard/algorithms/implementation/TimeConversion.java
@@ -0,0 +1,28 @@
+package com.hackerrank.dashboard.algorithms.implementation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class TimeConversion {
+
+	static String timeConversion(String s) throws ParseException {
+		Date parse =null;
+		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss a");
+		if(s.contains("PM")) {
+			 parse = format.parse(s.replace("PM", " PM"));
+		} else {
+			 parse = format.parse(s.replace("AM", " AM"));
+		}
+		format = new SimpleDateFormat("HH:mm:ss");
+		return format.format(parse);
+	}
+
+	public static void main(String[] args) throws ParseException {
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		String result = timeConversion(s);
+		System.out.println(result);
+	}
+}
---
+++ src/main/java/com/hackerrank/dashboard/java/introduction/JavaCurrencyFormatter.java
@@ -0,0 +1,31 @@
+package com.hackerrank.dashboard.java.introduction;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner scanner = new Scanner(System.in);
+		double payment = scanner.nextDouble();
+		scanner.close();
+
+		String us, india, china, france;
+		
+		DecimalFormat df = new DecimalFormat("0.00");
+		payment=(Double)df.parse(df.format(payment));
+
+		    france = NumberFormat.getNumberInstance(Locale.FRANCE).format(payment);
+		    us = NumberFormat.getNumberInstance(Locale.US).format(payment);
+		    china = NumberFormat.getNumberInstance(Locale.CHINA).format(payment);
+		    india = NumberFormat.getNumberInstance(Locale.US).format(payment);
+		
+		System.out.println("US: $" + us);
+		System.out.println("India: Rs." + india);
+		System.out.println("China: ¥" + china);
+		System.out.println("France: " + france +" €");
+	}
+}
