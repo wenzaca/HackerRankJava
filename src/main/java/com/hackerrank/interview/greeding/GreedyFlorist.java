3a7943a9bb46e3d6889a4a142baf3cefddf55598
Wendler Zacariotto <wenzaca@gmail.com>
2019-05-31 08:24:12 +0100

    Adding new challenge solution to greeding

---
+++ src/main/java/com/hackerrank/interview/greeding/GreedyFlorist.java
@@ -0,0 +1,51 @@
+package com.hackerrank.interview.greeding;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GreedyFlorist {
+    // Complete the getMinimumCost function below.
+    static int getMinimumCost(int k, int[] c) {
+        Arrays.sort(c);
+        int increasePrice = 0;
+        int result=0;
+        int people = k;
+        for(int i=c.length-1;i>=0;i--){
+            if(k>0){
+                k--;
+            } else{
+                increasePrice++;
+                k=people-1;
+            }
+            result+=(increasePrice + 1) * c[i];
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        System.out.println(getMinimumCost(k, c));
+
+        scanner.close();
+    }
+}
+
