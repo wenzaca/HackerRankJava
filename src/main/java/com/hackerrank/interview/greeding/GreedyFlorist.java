4c338e66da0ed1bf2311c7827be1fff79ea49b00
Wendler Zacariotto <wenzaca@gmail.com>
2019-05-31 08:30:03 +0100

    Adding new challenge solution to greeding with the tests

--- src/main/java/com/hackerrank/interview/greeding/GreedyFlorist.java
+++ src/main/java/com/hackerrank/interview/greeding/GreedyFlorist.java
@@ -1,7 +1,12 @@
 package com.hackerrank.interview.greeding;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class GreedyFlorist {
     // Complete the getMinimumCost function below.
@@ -46,6 +51,19 @@ public class GreedyFlorist {
         System.out.println(getMinimumCost(k, c));
 
         scanner.close();
+    }
+
+    @Test
+    public void testSolution() {
+        Assert.assertThat(getMinimumCost(3, new int[]{1,3,5,7,9}), is(29));
+        Assert.assertThat(getMinimumCost(2, new int[]{2,5,6}), is(15));
+        Assert.assertThat(getMinimumCost(3, new int[]{2,5,6}), is(13));
+        String[] s = "390225 426456 688267 800389 990107 439248 240638 15991 874479 568754 729927 980985 132244 488186 5037 721765 251885 28458 23710 281490 30935 897665 768945 337228 533277 959855 927447 941485 24242 684459 312855 716170 512600 608266 779912 950103 211756 665028 642996 262173 789020 932421 390745 433434 350262 463568 668809 305781 815771 550800".split(" ");
+        int [] arr = new int [50];
+        for(int i=0; i<50; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Assert.assertThat(getMinimumCost(3, arr), is(163578911));
     }
 }
 
